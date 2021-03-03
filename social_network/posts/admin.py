adab67672594df2b386978aad0615479bc84e887
parisaetemadinejad <parisaetemadinejad@gmail.com>
2021-03-03 17:31:59 +0330

    some function for post and comment and like and profile models created:)

--- social_network/posts/admin.py
+++ social_network/posts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from posts.models import Post, Comment, Like
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['content', 'author', 'created']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'post', 'created']
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['user', 'post', 'created']
--- social_network/profiles/apps.py
+++ social_network/profiles/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class ProfilesConfig(AppConfig):
     name = 'profiles'
+    verbose_name = 'Profiles, Relationships'
 
     def ready(self):
         import profiles.signals
