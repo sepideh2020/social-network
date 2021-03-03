5e387897f2dfde2c57e6b3b89ff5b82414b483b7
parisaetemadinejad <parisaetemadinejad@gmail.com>
2021-03-03 14:26:37 +0330

    post model created:)

--- social_network/posts/models.py
+++ social_network/posts/models.py
@@ -1,3 +1,14 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
-# Create your models here.
+from profiles.models import Profile
+
+
+class Post(models.Model):
+    content = models.TextField()
+    image = models.ImageField(upload_to='posts', validators=[FileExtensionValidator(['png', 'jpg', 'jpeg'])],
+                              blank=True)
+    liked = models.ManyToManyField(Profile, blank=True, related_name='likes')
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
