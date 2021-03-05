772a7d7914f1d6220d5ce7f0757b33296626551c
sepideh2020 <sepideniktab2020@gmail.com>
2021-03-06 01:21:07 +0330

    sending comment automatically added

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -5,5 +5,5 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', post_comment_create_and_list_view, name='main-post-view'),
-    path('liked/', like_unlike_post, name='like-post-view'),
+    path('liked/', like_unlike_post, name='like-post-view'),  #????
 ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,17 +1,50 @@
 from django.shortcuts import render, redirect
 from .models import Post, Like
 from profiles.models import Profile
+from .forms import PostModelForm, CommentModelForm
 
 
 # Create your views here.
 
 def post_comment_create_and_list_view(request):
     qs = Post.objects.all()
+    p_form = PostModelForm(request.POST, request.FILES)
+    # before saving form we need to have author to be assigned to the field of author in the post model so
+    # we need to get the profile be request.user
+
+    # initial
+    p_form = PostModelForm()
+    # we used request.Files because we might send an image
+    c_form = CommentModelForm()
+    post_added = False
     profile = Profile.objects.get(user=request.user)
+
+    if 'submit_p_form' in request.POST:
+        print(request.POST)
+        p_form = PostModelForm(request.POST, request.FILES)
+
+        if p_form.is_valid():
+            instance = p_form.save(commit=False)
+            instance.author = profile
+            instance.save()
+            p_form = PostModelForm()  # our form loads again for writing a new post
+            post_added = True
+    if 'submit_c_form' in request.POST:
+        c_form = CommentModelForm(request.POST)
+        if c_form.is_valid():
+            instance = c_form.save(commit=False)
+            instance.user = profile
+            instance.post = Post.objects.get(id=request.POST.get('post_id'))  # ???
+            instance.save()
+            c_form = CommentModelForm()  # resets the form
 
     context = {
         'qs': qs,
         'profile': profile,
+        'p_form': p_form,
+        'c_form': c_form,
+        'post_added': post_added,
+
     }
     return render(request, 'posts/main.html', context)
 
