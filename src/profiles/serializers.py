d336f2ac137fb9bdb6c1199e5e14dd7f97002276
sepideh2020 <sepideniktab2020@gmail.com>
2021-03-24 21:32:51 +0430

    pulled

---
+++ src/profiles/serializers.py
@@ -0,0 +1,12 @@
+
+
+from rest_framework import serializers
+
+import profiles
+from profiles.models import CustomUser
+
+
+# class PhoneNumberSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = CustomUser
+#         fields = '__all__'
--- src/social_network/settings.py
+++ src/social_network/settings.py
@@ -42,14 +42,14 @@ INSTALLED_APPS = [
 ]
 
 AUTHENTICATION_BACKENDS = (
-    'profiles.backends.PhoneEmailBackend',  #
+    'profiles.backends.PhoneEmailBackend',  
     'django.contrib.auth.backends.ModelBackend',
 
 )
 
-AUTH_USER_MODEL = 'profiles.CustomUser'  #
-LOGIN_REDIRECT_URL = '../posts/'  #
-LOGOUT_REDIRECT_URL = '/login/'  #
+AUTH_USER_MODEL = 'profiles.CustomUser'
+LOGIN_REDIRECT_URL = '../posts/'
+LOGOUT_REDIRECT_URL = '/login/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -138,11 +138,3 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
-
-
-#email
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'sepideniktab1996@gmail.com'
-EMAIL_HOST_PASSWORD = 'germany2022'
-EMAIL_PORT = 587
