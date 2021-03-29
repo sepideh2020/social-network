b259b16972b5b18ca56d71779d9cb3520fa041d1
parisaetemadinejad <parisaetemadinejad@gmail.com>
2021-03-30 00:07:09 +0430

    validator added

--- src/profiles/forms.py
+++ src/profiles/forms.py
@@ -2,13 +2,11 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.core.exceptions import ValidationError
 
-import re
-
-from utility.constant import WEBSITE_REGEX
 from .models import CustomUser
 
 
 class SignUpForm(UserCreationForm):
+
     class Meta:
         model = CustomUser
         fields = (
@@ -29,9 +27,3 @@ class ProfileModelForm(forms.ModelForm):
     class Meta:
         model = CustomUser
         fields = ('first_name', 'last_name', 'bio', 'website', 'avatar')
-
-    # def clean(self):
-    #     cleaned_data = super().clean()
-    #     pattern = WEBSITE_REGEX
-    #     if not re.search(pattern, cleaned_data.get('website')):
-    #         raise ValidationError('your website is invalid')
