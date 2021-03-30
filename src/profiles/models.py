efad9f1262602acd5d93fde309fd527c743ce992
parisaetemadinejad <parisaetemadinejad@gmail.com>
2021-03-30 16:00:52 +0430

    solved create superuser bug

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -14,21 +14,21 @@ class CustomUserManager(BaseUserManager):
     """
     use_in_migrations = True
 
-    def _create_user(self, user_name, password, **extra_fields):
-        user = self.model(user_name=user_name, **extra_fields)
+    def _create_user(self, username, password, **extra_fields):
+        user = self.model(username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, user_name, password=None, **extra_fields):
+    def create_user(self, username, password=None, **extra_fields):
         extra_fields.setdefault('is_superuser', False)
         extra_fields.setdefault('is_staff', False)
-        return self._create_user(user_name, password, **extra_fields)
+        return self._create_user(username, password, **extra_fields)
 
-    def create_superuser(self, password, user_name, **extra_fields):
+    def create_superuser(self, password, username, **extra_fields):
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_staff', True)
-        return self._create_user(user_name, password, **extra_fields)
+        return self._create_user(username, password, **extra_fields)
 
     def get_all_profiles_to_invite(self, sender):
 
